94859ba2c91ac39d75feecc4dc8f9b7af261e255
Blaise Laflamme <blaise@laflamme.org>
2016-05-23 23:07:41 -0400

    add users paths

--- morepath_services/paths.py
+++ morepath_services/paths.py
@@ -1,7 +1,28 @@
 from .app import App
-from .models import Root
+from .models import (
+    Root,
+    Users,
+    User
+    )
 
 
 @App.path(model=Root, path='/')
 def get_root():
     return Root()
+
+
+@App.path(model=Users, path='/users')
+def get_users(request):
+    service = request.app.find_service('users')
+    users = service.all()
+    return Users(users)
+
+
+@App.path(
+    model=User,
+    path='/users/{id}',
+    converters={'id': int}
+    )
+def get_user(request, id):
+    service = request.app.find_service('users')
+    return service.by_id(id=id)
