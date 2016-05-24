e9ee973236256989f14299ade188a05c3a38fec3
Blaise Laflamme <blaise@laflamme.org>
2016-05-23 23:07:52 -0400

    add users views

--- morepath_services/views.py
+++ morepath_services/views.py
@@ -1,7 +1,26 @@
 from .app import App
-from .models import Root
+from .models import (
+    Root,
+    Users,
+    User
+    )
 
 
 @App.json(model=Root)
 def root_view(self, request):
     return {'name': 'root'}
+
+
+@App.json(model=Users)
+def users_view(self, request):
+    return {
+        'users': [request.view(user) for user in self.users]
+        }
+
+
+@App.json(model=User)
+def user_view(self, request):
+    return {
+        'username': self.username,
+        'email': self.email
+        }
