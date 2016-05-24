ac253e0fd367b0f3613d6ffe7750e4d46a03a2af
Blaise Laflamme <blaise@laflamme.org>
2016-05-23 23:07:03 -0400

    add users models

--- morepath_services/models.py
+++ morepath_services/models.py
@@ -1,3 +1,23 @@
+from sqlalchemy import (
+    Column,
+    Integer,
+    Text
+    )
+from .db import Base
+
 
 class Root(object):
     pass
+
+
+class Users(object):
+    def __init__(self, users):
+        self.users = users
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(Text)
+    email = Column(Text)
