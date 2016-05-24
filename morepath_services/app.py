2ee4d5e46aae91cc6c9342191fbbc604730de6ab
Blaise Laflamme <blaise@laflamme.org>
2016-05-23 23:06:17 -0400

    add sqlalchemy settings

--- morepath_services/app.py
+++ morepath_services/app.py
@@ -4,3 +4,10 @@ from more.services import ServiceApp
 
 class App(TransactionApp, ServiceApp):
     pass
+
+
+@App.setting_section(section='sqlalchemy')
+def get_sqlalchemy_setting_section():
+    return {
+        'sqlalchemy.url': 'sqlite:///morepath_services.db'
+        }
